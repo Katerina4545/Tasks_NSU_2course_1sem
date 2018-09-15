8619c8f208b93ff615d61f08ac4c555e7edab5ad
Katerina4545 <42957294+Katerina4545@users.noreply.github.com>
2018-09-15 14:26:09 +0700

    Add files via upload

---
+++ test_0B/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+
+#define MAX_LENGHT 50
+using namespace std; 
+
+int main(int argc, char** argv)
+{
+	ifstream in(argv[1]);
+	ofstream out(argv[2]);
+
+	map<string, float>map_;
+	char* str = new char;
+	float kol = 0;
+	while (in.getline(str, MAX_LENGHT, ' '))
+	{
+		if (map_.find(str) == map_.end())		//если str нет в map_
+			map_.insert(make_pair(str, 1));		
+		else 
+			map_[str]++;
+		kol++;								//всего слов
+	}
+
+
+	//вывод
+	out << "word" << "," << "frequency" << "," << "frequency%" << endl;
+	if (kol!=0)
+	{
+		map<string, float>::iterator it = map_.end();
+		while (it!= map_.begin())
+		{
+			it--;
+			out << fixed;
+			out.precision(0);
+			out << it->first << "," << it->second << ",";
+			
+			float a = it->second / kol;
+			out << fixed;
+			out.precision(2);
+			out << a * 100 << endl;			
+		}
+	}
+	in.close();
+	out.close();
+	return 0;
+}
